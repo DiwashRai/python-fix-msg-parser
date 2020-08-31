1479763b6349501a46ed12968c119c41cc46bf68
Diwash Rai <diwash.rai1993@gmail.com>
2020-08-31 02:34:30 +0100

    added sample dictrionary, input file and output file. updated readme

--- parse-fix-msg.py
+++ parse-fix-msg.py
@@ -6,17 +6,16 @@ field_name_column_length = 23
 trailing_column_length = 50
 msg_separator_length = 100
 
-data_dictionary_file = "data-dictionary.xml"
+data_dictionary_file = "FIX44.xml"
 input_file_name = "input.txt"
 output_file_name = "output.txt"
 
 print_enums = True
+use_row_separators = False
 
 row_char = "-"
 
-tag_dictionary = {}
-
-def build_row_separator():
+def build_row_separators():
     global row_separator
     global msg_separator
     row_separator = ""
@@ -35,42 +34,62 @@ def build_row_separator():
     msg_separator += "\n"
 
 
-
-def populate_tag_dictionary():
+def init_fields():
+    global fields
     tree = ET.parse(data_dictionary_file)
     root = tree.getroot()
 
     for child in root:
         if child.tag == "fields":
             fields = child
-    for field in fields:
-        tag_dictionary[field.get("number")] = field.get("name")
+
+
+def get_enum_str(tag_number):
+    enums = ""
+    field = fields.find(f"./field[@number='{tag_number}']")
+    enum_padding = len(row_separator) - (tag_column_length + 3 + field_name_column_length + 3 + 1)
+
+    if field:
+        for i in range(enum_padding):
+            enums += " "
+        for enum in field:
+            enums += enum.get("enum") + " : " + enum.get("description") + ", "
+    return enums
+
 
 def make_readable(tags, output_file):
-    output_file.write(row_separator)
+    if use_row_separators:
+        output_file.write(row_separator)
 
     for tag in tags:
         pair = tag.split("=")
         if len(pair) == 1:
             continue
 
-        padding1_len = tag_column_length - len(pair[0])
+        tag_number = pair[0]
+        tag_name = fields.find(f"./field[@number='{tag_number}']").get("name")
+        tag_value = pair[1]
+
+        padding1_len = tag_column_length - len(tag_number)
         padding1 = ""
-
-        padding2_len = field_name_column_length - len(tag_dictionary[pair[0]])
-        padding2 = ""
-
         if padding1_len > 0:
             for i in range(padding1_len):
                 padding1 = padding1 + " "
 
+        padding2_len = field_name_column_length - len(tag_name)
+        padding2 = ""
         if padding2_len > 0:
             for i in range(padding2_len):
                 padding2 = padding2 + " "
 
-        line = pair[0] + padding1 + " | " + tag_dictionary[pair[0]] + padding2 + " | " + pair [1] + "\n"
+        enums = ""
+        if print_enums:
+            enums = get_enum_str(tag_number)
+
+        line = tag_number + padding1 + " | " + tag_name + padding2 + " | " + tag_value + enums + "\n"
         output_file.write(line)
-        output_file.write(row_separator)
+        if use_row_separators:
+            output_file.write(row_separator)
 
 
 def parse_input_file():
@@ -92,8 +111,8 @@ def parse_input_file():
 
 
 def main():
-    build_row_separator()
-    populate_tag_dictionary()
+    build_row_separators()
+    init_fields()
     parse_input_file()
 
 
